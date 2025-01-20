1cfc1937f908411a53ccfd746e14f0b977b84246
Petter Olsen <petol@users.noreply.github.com>
2025-01-20 14:13:40 +0100

    Create stg_customer_location.sql

---
+++ models/staging/stg_customer_location.sql
@@ -0,0 +1,20 @@
+with source as (
+
+    {#-
+    Normally we would select from the table here, but we are using seeds to load
+    our data in this project
+    #}
+    select * from {{ ref('raw_customerloc') }}
+
+),
+
+renamed as (
+
+    select
+        id as customer_id,
+        region as location
+    from source
+
+)
+
+select * from renamed
